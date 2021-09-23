b521a73a05fee88a135223fed2f9636e5a8c0ad5
burcMnt <burcumantar0@gmail.com>
2021-09-23 13:32:15 +0300

    ef, ef config, ef repository

--- README.md
+++ README.md
@@ -5,6 +5,9 @@ https://github.com/dotnet-architecture/eShopOnWeb.
 ## Web
 
 ## Infrastructure
+Install-Package Microsoft.EntityFrameworkCore
+Install-Package Npgsql.EntityFrameworkCore.PostgreSQL
+
 
 ## ApplicationCore
 Install-Package Ardalis.Specification
---
+++ src/Infrastructure/Data/EFRepository.cs
@@ -0,0 +1,68 @@
+﻿using ApplicationCore.Entities;
+using ApplicationCore.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Data
+{
+    public class EFRepository<T> : IAsyncRepository<T> where T : BaseEntity
+    {
+        private readonly ApplicationDbContext _context;
+
+        public EFRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<T> AddAsync(T entity)
+        {
+            _context.Add(entity);
+            await _context.SaveChangesAsync();
+            return entity;
+        }
+
+        public Task CountAsync(Ardalis.Specification.ISpecification<T> spec)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task DeleteAsync(T entity)
+        {
+            _context.Remove(entity);
+            await _context.SaveChangesAsync();
+        }
+
+        public Task<T> FirstAsync(Ardalis.Specification.ISpecification<T> spec)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T> FirstOrDefaultAsync(Ardalis.Specification.ISpecification<T> spec)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<T> GetByIdAsync(int id)
+        {
+            return await _context.Set<T>().FindAsync(id);
+        }
+
+        public async Task<List<T>> ListAllAsync()
+        {
+            return await _context.Set<T>().ToListAsync();
+        }
+
+        public Task<List<T>> ListAllAsync(Ardalis.Specification.ISpecification<T> spec)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task UpdateAsync(T entity)
+        {
+            _context.Update(entity);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
