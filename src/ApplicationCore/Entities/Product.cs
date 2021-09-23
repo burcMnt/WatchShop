0192961f0929bedca92b2f0a51f93566706e62df
burcMnt <burcumantar0@gmail.com>
2021-09-23 10:56:07 +0300

    Entities

---
+++ src/ApplicationCore/Entities/Product.cs
@@ -0,0 +1,16 @@
+﻿namespace ApplicationCore.Entities
+{
+    public class Product
+    {
+        public string ProductName { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public string PictureUri { get; set; }
+
+        public int CategoryId { get; set; }
+        public Category Category { get; set; }
+
+        public int BrandId { get; set; }
+        public Brand Brand { get; set; }
+    }
+}
